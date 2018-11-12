2bc442c0e5e468a600111cee95a7a87f22b13739
Joe <jose.ques@southworks.com>
2018-11-12 17:17:18 -0300

    WIP base models port

--- Java/libraries/recognizers-text-choice/src/main/java/com/microsoft/recognizers/text/choice/models/BooleanModel.java
+++ Java/libraries/recognizers-text-choice/src/main/java/com/microsoft/recognizers/text/choice/models/BooleanModel.java
@@ -1,11 +1,16 @@
 package com.microsoft.recognizers.text.choice.models;
 
-import java.util.Map;
+import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 import com.microsoft.recognizers.text.IExtractor;
 import com.microsoft.recognizers.text.IParser;
 import com.microsoft.recognizers.text.ParseResult;
 import com.microsoft.recognizers.text.choice.Constants;
+import com.microsoft.recognizers.text.choice.parsers.OptionsOtherMatchParseResult;
+import com.microsoft.recognizers.text.choice.parsers.OptionsParseDataResult;
 
 public class BooleanModel extends ChoiceModel {
 
@@ -18,7 +23,22 @@ public class BooleanModel extends ChoiceModel {
 		return Constants.MODEL_BOOLEAN;
 	}
 
-	protected Map<String, Object> GetResolution(ParseResult parseResult) {
-		throw new UnsupportedOperationException();
+	protected SortedMap<String, Object> GetResolution(ParseResult parseResult) {
+		OptionsParseDataResult data = (OptionsParseDataResult) parseResult.data;
+		SortedMap<String, Object> results = new TreeMap<String, Object>();
+		results.put("value", parseResult.value);
+		results.put("score", data.score);
+		List<Object> otherMatchesResult = data.otherMatches.stream().map(otherMatch -> makeOtherMatchesObject(otherMatch)).collect(Collectors.toList());
+		results.put("otherResults", otherMatchesResult);
+
+		return results;
+	}
+
+	private SortedMap<String, Object> makeOtherMatchesObject(OptionsOtherMatchParseResult matchParseResult) {
+		SortedMap<String, Object> results = new TreeMap<String, Object>();
+		results.put("text", matchParseResult.text);
+		results.put("value", matchParseResult.value);
+		results.put("score", matchParseResult.score);
+		return results;
 	}
 }
--- Java/libraries/recognizers-text-choice/src/main/java/com/microsoft/recognizers/text/choice/models/ChoiceModel.java
+++ Java/libraries/recognizers-text-choice/src/main/java/com/microsoft/recognizers/text/choice/models/ChoiceModel.java
@@ -3,6 +3,7 @@ package com.microsoft.recognizers.text.choice.models;
 import java.util.List;
 import java.util.Map;
 
+import com.microsoft.recognizers.text.ExtractResult;
 import com.microsoft.recognizers.text.IExtractor;
 import com.microsoft.recognizers.text.IModel;
 import com.microsoft.recognizers.text.IParser;
@@ -10,26 +11,29 @@ import com.microsoft.recognizers.text.ModelResult;
 import com.microsoft.recognizers.text.ParseResult;
 import com.microsoft.recognizers.text.choice.Constants;
 
-public class ChoiceModel implements IModel {
-	protected IExtractor Extractor;
-	protected IParser Parser;
-	
-	public ChoiceModel(IParser parser, IExtractor extractor) {
-		this.Parser = parser;
-		this.Extractor = extractor;
+public abstract class ChoiceModel implements IModel {
+	protected IExtractor extractor;
+	protected IParser parser;
+	public String modelTypeName;
+	public ChoiceModel(IParser Parser, IExtractor Extractor) {
+		parser = Parser;
+		extractor = Extractor;
+		modelTypeName = Constants.MODEL_BOOLEAN;
 	}
 
 	@Override
 	public String getModelTypeName() {
-		return Constants.MODEL_BOOLEAN;
+		return modelTypeName;
 	}
 
 	@Override
 	public List<ModelResult> parse(String query) {
-		throw new UnsupportedOperationException();
+		List<ExtractResult> extractResults = extractor.extract(query);
+		List<ParseResult> parseResults = extractResults.stream().map(extrRes->parser.parse(extrRes)).collect(Collectors.toList());
+		
+		List<ModelResult> modelResultList = parseResults.stream().map(parseRes->new ModelResult(parseRes.Start.Value, parseRes.Start.Value + parseRes.Length.Value - 1, GetResolution(parseRes), parseRes.Text, modelTypeName).collect(Collectors.toList());;
+		return parseResults.Select();
 	}
 
-	protected Map<String, Object> GetResolution(ParseResult parseResult) {
-		throw new UnsupportedOperationException();
-	}
+	protected abstract Map<String, Object> GetResolution(ParseResult parseResult);
 }
