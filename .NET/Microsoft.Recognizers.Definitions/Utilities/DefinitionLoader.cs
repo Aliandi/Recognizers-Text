4d23aac892d5f24e97549a82c215d5c55b9a1ed0
aliandi <mica.andini@gmail.com>
2019-01-18 14:35:46 -0300

    Fix all the warnings in Definitions - Remove trailing whitespace - Add trailing comma - Reove and add spacing as needed

--- .NET/Microsoft.Recognizers.Definitions/Utilities/DefinitionLoader.cs
+++ .NET/Microsoft.Recognizers.Definitions/Utilities/DefinitionLoader.cs
@@ -1,29 +1,30 @@
-﻿using System.Collections.Generic;
+﻿// <copyright file="AssemblyInfo.cs" company="Microsoft">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
 namespace Microsoft.Recognizers.Definitions.Utilities
 {
     public static class DefinitionLoader
     {
-
         public static Dictionary<Regex, Regex> LoadAmbiguityFilters(Dictionary<string, string> filters)
         {
             var ambiguityFiltersDict = new Dictionary<Regex, Regex>();
 
-            if (filters != null) {
-
+            if (filters != null)
+            {
                 foreach (var item in filters)
                 {
                     if (!"null".Equals(item.Key))
                     {
-                        ambiguityFiltersDict.Add(new Regex(item.Key, RegexOptions.Singleline),
-                                                 new Regex(item.Value, RegexOptions.Singleline));
+                        ambiguityFiltersDict.Add(new Regex(item.Key, RegexOptions.Singleline), new Regex(item.Value, RegexOptions.Singleline));
                     }
                 }
             }
 
             return ambiguityFiltersDict;
         }
-
     }
 }
